de003a5a8ba3428a8a5070c5b82b8b30c5f738bd
Masum Islam <masum.islam@metropolia.com>
2018-09-18 23:04:33 +0300

    fix: New Android security bypass fix

--- app/src/main/java/com/home/ma/photolocationnote/CameraActivity.java
+++ app/src/main/java/com/home/ma/photolocationnote/CameraActivity.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
@@ -47,6 +48,9 @@ public class CameraActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // To bypass the new Android security model
+         StrictMode.VmPolicy.Builder newbuilder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(newbuilder.build());
         setContentView(R.layout.activity_camera);
         // Checking camera availability
         if (!isDeviceSupportCamera()) {
